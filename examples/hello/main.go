e5d8ca45667b50feda6cb88f5d87e0e55fe6feef
Gary Burd <gary@beagledreams.com>
2011-07-07 21:03:58 -0700

    Add .gitignore
    Set TARG=example in all Mxample makefiles.
    Add helll world example.

---
+++ examples/hello/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"github.com/garyburd/twister/server"
+	"github.com/garyburd/twister/web"
+	"io"
+)
+
+func serveHello(req *web.Request) {
+	w := req.Respond(web.StatusOK, web.HeaderContentType, "text/plain; charset=\"utf-8\"")
+	io.WriteString(w, "Hello World!")
+}
+
+func main() {
+	h := web.NewRouter().Register("/", "GET", serveHello)
+	server.Run(":8080", h)
+}
