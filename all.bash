85db55d401b3fe80720ec75d7d0efefc9060b81f
Gary Burd <gary@beagledreams.com>
2012-01-25 12:51:45 -0800

    Remove Makefiles.
    The new "go" command replaces Makefiles.

--- all.bash
+++ all.bash
@@ -1,6 +1,3 @@
 #!/usr/bin/env bash
-
-for dir in web server websocket expvar pprof adapter examples/demo examples/chat examples/wiki
-do
-    (cd $dir && pwd && make DEPS= $*)
-done
+B=github.com/garyburd/twister
+go $* $B/web $B/server $B/websocket $B/expvar $B/pprof $B/adapter $B/examples/demo $B/examples/chat $B/examples/wiki
